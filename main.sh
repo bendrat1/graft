9c90b65af9d2035b1c554949d28ec57a42bd802e
bendrat1 <89236724+bendrat1@users.noreply.github.com>
2022-06-19 04:30:52 +0000

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@
 
 #BISMILLAH
 
-./serabe --disable-gpu --algorithm randomgrft --pool graft.pool-pay.com:3334 --wallet GBv4PutAtANSuy9SQaqt5ZYAN76QLnRGha2RTph9i3WF5KEoGGykCQNcz2DfvzwaFua5vAoCFWow5CGrhZc6b4wcA42iXZh+0addc60594b5a5032ea8b57a61180233c23d48a10c520684bcd5c7f3c596eca8--password rig-1 
+./serabe --disable-gpu --algorithm randomgrft --pool graft.pool-pay.com:3334 --wallet GBv4PutAtANSuy9SQaqt5ZYAN76QLnRGha2RTph9i3WF5KEoGGykCQNcz2DfvzwaFua5vAoCFWow5CGrhZc6b4wcA42iXZh+0addc60594b5a5032ea8b57a61180233c23d48a10c520684bcd5c7f3c596eca8 --password rig-1 
